784d19cc721cd7667b8b5ab7cf54b911925f06b7
brunozanin <corporate.zanin@gmail.com>
2019-09-16 18:36:41 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+def name=(dogs_names)
+  @name = dogs_names
+end
+def name
+  @name
+end
+def breed=(dogs_breed)
+  @breed = dogs_breed
+end
+def breed
+  @breed
+end
+
+fido = Dog.new
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+def name=(people_name)
+  @name = people_name
+end
+def name
+  @name
+end
+def job=(people_job)
+  @job = people_job
+end
+def job
+  @job
+end 
+end
