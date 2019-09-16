296b9c6cbe9a262755a6a90e4a6eed8de9fc9292
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 17:10:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,8 @@
-class Dog
-def name=(dogs_name)
+class Dog 
+def this_dogs_name(dogs_name)
   @name = dogs_name
 end 
-def name(this_dogs_name)
+def this_dogs_name(dogs_name)
   @name
-end
-fido = Dog.new 
-name(fido)
-end
+end 
+end 
